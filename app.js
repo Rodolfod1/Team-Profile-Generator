8923d779a4cd0815216f4539a6fc9a5850f706cb
Rodolfo Diaz <rodolfodzr@gmail.com>
2020-11-15 00:13:02 -0500

    adding the main app configuration using inquirer

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
+const util = require("util");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -12,14 +13,19 @@ const render = require("./lib/htmlRenderer");
 const Choice = require("inquirer/lib/objects/choice");
 const Choices = require("inquirer/lib/objects/choices");
 
-function prompter(){
-    return inquirer.prompt([
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+const employees=[];
+var again=true;
+
+function askQs(){
+    inquirer.prompt([
         {   type:"checkbox",
             message:"Select Role",
             Choices:[
                 "Manager",
                 "Engineer",
-                "Employee",
                 "Intern"
             ]
     },
@@ -31,32 +37,76 @@ function prompter(){
     {
         type:"input",
         message:"Please Provide the e-mail: ",
-        name:"name"
+        name:"email"
     },
     {
         type:"input",
-        message:"Please Provide the e-mail: ",
-        name:"name"
+        message:"Please Provide employee id#: ",
+        name:"id"
     },
+    ]).then(answer => {
+        // check for the role and create objects accordingly 
+         if (answer.Choices === "Manager") {
+                inquirer.prompt([
+                    {
+                        type:"input",
+                        message:"Please provide the Office Number: ",
+                        name:"officeNumber"
+                    }
+                ]).then(val =>{
+                    /// creates an object with all variables and push it to employees 
+                    employees.push(new Manager(answer.name,answer.id,answer.email,val.officeNumber));
+                })
+
+            }else if (answer.Choices=== "Engineer" ){
+                inquirer.prompt([
+                    {
+                        type:"input",
+                        message:"Please provide a Github username: ",
+                        name:"gitHub"
+                    }
+                ]).then(val =>{
+                    /// creates an object with all variables and push it to employees 
+                    employees.push(new Engineer(answer.name,answer.id,answer.email,val.gitHub));
+                })
+
+            }else {
+                inquirer.prompt([
+                    {
+                        type:"input",
+                        message:"Please provide the School Name: ",
+                        name:"school"
+                    }
+                ]).then(val =>{
+                    /// creates an object with all variables and push it to employees 
+                    employees.push(new Intern(answer.name,answer.id,answer.email,val.school));
+                })
 
 
-
-
-
-    ])
+            };
+    })
 }
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+
+
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+const html= render(employees);
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
+
+
+
+
+
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
